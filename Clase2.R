9012ea871cf41103a9555c24c0bb43bb345427c6
Rodrigo Sanchez <robotabogado@gmail.com>
2021-05-24 18:02:01 -0400

    Final de la clase

--- Clase2.R
+++ Clase2.R
@@ -8,3 +8,12 @@ plants <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tid
 actions <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-08-18/actions.csv")
 
 threats <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-08-18/threats.csv")
+
+data("msleep")
+
+Tabla <- msleep %>% 
+  group_by(vore) %>% 
+  summarise_at("sleep_total",.funs = list(Mean = mean, SD = sd)) %>% 
+  dplyr::filter(!is.na(vore)) %>% 
+  arrange(desc(Mean))
+
